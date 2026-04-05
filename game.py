953b65e27529a2c0083de2553cf1c5cd6aa517ea
GuilhermeMaglia <guipmagliad@gmail.com>
2026-04-05 00:46:40 -0300

    Criação dos tiros

--- game.py
+++ game.py
@@ -33,9 +33,13 @@ class nave(pygame.sprite.Sprite):
         self.rect.center = [x, y]
         self.health_start = health
         self.health_remaining = health
+        self.last_shot = pygame.time.get_ticks()
     
     def update(self):
         speed = 8
+
+        #Variavel cooldown
+        cooldown = 500 #milisegundos
 
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT] and self.rect.left > 0:
@@ -43,14 +47,38 @@ class nave(pygame.sprite.Sprite):
         if key[pygame.K_RIGHT] and self.rect.right < screen_width:
             self.rect.x += speed
 
+
+        # cooldown
+        time_now = pygame.time.get_ticks()
+
+        #atirar
+        if key[pygame.K_SPACE] and time_now - self.last_shot > cooldown:
+            balas = Balas(self.rect.centerx, self.rect.top)
+            balas_grupo.add(balas)
+            self.last_shot = time_now
+
+
         # Desenha barra de vida
         pygame.draw.rect(screen, red, (self.rect.x, (self.rect.bottom + 10), self.rect.width, 15 ))
         if self.health_remaining > 0:
             pygame.draw.rect(screen, green, (self.rect.x, (self.rect.bottom + 10), int(self.rect.width * (self.health_remaining / self.health_start)), 15 ))
 
+# Classe Balas
+class Balas(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("img/bullet.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
 
-# Criação do grupo sprite
+    def update(self):
+        self.rect.y -= 5
+        if self.rect.bottom < 0:
+            self.kill()
+
+# Criação do grupo sprites
 nave_grupo = pygame.sprite.Group()
+balas_grupo = pygame.sprite.Group()
 
 # Criação do player
 nave = nave(int(screen_width / 2), screen_height - 100, 3)
@@ -70,8 +98,11 @@ while run:
     
     nave.update()
 
+    balas_grupo.update()
+
     # Atualiza o grupo sprites
     nave_grupo.draw(screen)
+    balas_grupo.draw(screen)
 
 
     pygame.display.update()
